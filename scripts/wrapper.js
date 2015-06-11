48fe33210672a329182aca91c8b0a52ac91def3a
류형욱 <mintrupt@mintrupt.com>
2015-06-12 06:08:02 +0900

    사소한 편집

--- scripts/wrapper.js
+++ scripts/wrapper.js
@@ -4,7 +4,6 @@ function wrap(obj, func) {
     var args = Array.prototype.slice.call(arguments);
     var callback = args[args.length - 1];
     co(function* () { return yield func.apply(obj, args); }).then(success, error);
-
     function success(result) {
       callback(null, result);
     }
